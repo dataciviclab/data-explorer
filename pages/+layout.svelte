caf77484783f4141e9960a9cf9c12b95b1e06b79
Matteo <matteo.cavo.data@gmail.com>
2026-03-25 12:49:08 +0100

    fix: aggiunge il wordmark del data explorer (#22)
    * fix: aggiunge il wordmark del data explorer
    
    * fix: aggiunge la favicon del data explorer

--- pages/+layout.svelte
+++ pages/+layout.svelte
@@ -2,10 +2,12 @@
 	import '@evidence-dev/tailwind/fonts.css';
 	import '../app.css';
 	import { EvidenceDefaultLayout } from '@evidence-dev/core-components';
+	import lightLogo from './assets/dataciviclab-wordmark-light.svg';
+	import darkLogo from './assets/dataciviclab-wordmark-dark.svg';
 
 	export let data;
 </script>
 
-<EvidenceDefaultLayout {data} title="DataCivicLab" builtWithEvidence={false}>
+<EvidenceDefaultLayout {data} {lightLogo} {darkLogo} builtWithEvidence={false}>
 	<slot slot="content" />
 </EvidenceDefaultLayout>
