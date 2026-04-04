fff7b6e2cef6fcad601f2599d0257b78495516c8
Zio Gabber <78922322+Gabrymi93@users.noreply.github.com>
2026-04-04 13:56:12 +0100

    Porta dipendenti_pubblici in cache locale nel data explorer (#45)

--- sources/dipendenti_pubblici/occupazione.sql
+++ sources/dipendenti_pubblici/occupazione.sql
@@ -7,9 +7,9 @@ SELECT
   cessati_totali
 FROM read_parquet(
   [
-    'https://storage.googleapis.com/dataciviclab-clean/dipendenti_pubblici_2021_2023/2021/dipendenti_pubblici_2021_2023_2021_clean.parquet',
-    'https://storage.googleapis.com/dataciviclab-clean/dipendenti_pubblici_2021_2023/2022/dipendenti_pubblici_2021_2023_2022_clean.parquet',
-    'https://storage.googleapis.com/dataciviclab-clean/dipendenti_pubblici_2021_2023/2023/dipendenti_pubblici_2021_2023_2023_clean.parquet'
+    '.evidence/gcs-cache/dipendenti_pubblici_2021_2023/2021/dipendenti_pubblici_2021_2023_2021_clean.parquet',
+    '.evidence/gcs-cache/dipendenti_pubblici_2021_2023/2022/dipendenti_pubblici_2021_2023_2022_clean.parquet',
+    '.evidence/gcs-cache/dipendenti_pubblici_2021_2023/2023/dipendenti_pubblici_2021_2023_2023_clean.parquet'
   ]
 )
 WHERE comparto IS NOT NULL
