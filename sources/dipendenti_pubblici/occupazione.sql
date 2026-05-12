5f057a4b5220156e79e3717085c5468808f25cb2
Zio Gabber <78922322+Gabrymi93@users.noreply.github.com>
2026-05-12 17:53:46 +0100

    refactor: source dipendenti_pubblici legge da GCS diretto

--- sources/dipendenti_pubblici/occupazione.sql
+++ sources/dipendenti_pubblici/occupazione.sql
@@ -6,10 +6,27 @@ SELECT
   assunti_totali,
   cessati_totali
 FROM read_parquet(
-  [
-    '.evidence/gcs-cache/dipendenti_pubblici_2021_2023/2021/dipendenti_pubblici_2021_2023_2021_clean.parquet',
-    '.evidence/gcs-cache/dipendenti_pubblici_2021_2023/2022/dipendenti_pubblici_2021_2023_2022_clean.parquet',
-    '.evidence/gcs-cache/dipendenti_pubblici_2021_2023/2023/dipendenti_pubblici_2021_2023_2023_clean.parquet'
-  ]
+  'https://storage.googleapis.com/dataciviclab-clean/dipendenti_pubblici/2021/dipendenti_pubblici_2021_clean.parquet'
 )
-WHERE comparto IS NOT NULL
+UNION ALL
+SELECT
+  CAST(anno AS VARCHAR) AS anno,
+  comparto,
+  donne_totali,
+  uomini_totali,
+  assunti_totali,
+  cessati_totali
+FROM read_parquet(
+  'https://storage.googleapis.com/dataciviclab-clean/dipendenti_pubblici/2022/dipendenti_pubblici_2022_clean.parquet'
+)
+UNION ALL
+SELECT
+  CAST(anno AS VARCHAR) AS anno,
+  comparto,
+  donne_totali,
+  uomini_totali,
+  assunti_totali,
+  cessati_totali
+FROM read_parquet(
+  'https://storage.googleapis.com/dataciviclab-clean/dipendenti_pubblici/2023/dipendenti_pubblici_2023_clean.parquet'
+)
